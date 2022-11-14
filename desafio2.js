172019bc2a682d48d7b11a9ca6ac185c2130a96c
Alexsandro Moura <91920947+alexsandro-moura@users.noreply.github.com>
2022-11-14 17:50:49 -0300

    Update desafio2.js

--- desafio2.js
+++ desafio2.js
@@ -1,4 +1,4 @@
-/*Respondendo ao desafio 2 proposto na página do Instagram do @fabiocberg Fábio Bergmann*/
+//Respondendo ao desafio 2 proposto na página do Instagram do @fabiocberg Fábio Bergmann
 
 /*O objetivo do desafio é criar uma função que receba um parâmetro
 e retorne o tipo de dado passado com parâmetro*/
